514b379fdea5e283655afb77f0d95225554d0b15
tadashi-aikawa <syou.maman@gmail.com>
2020-06-24 17:35:30 +0900

    🔨 Specify maxWorkers=1 (jest)

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 // eslint-disable-next-line no-undef
 module.exports = {
   verbose: true,
+  maxWorkers: 1,
   collectCoverage: true,
   collectCoverageFrom: ["src/**/*.ts", "sample/**/*.ts"],
   testPathIgnorePatterns: ["/node_modules/", "/dist/"],
