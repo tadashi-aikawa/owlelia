727c3219f63fd79190f8396936d0e054b77fd3a9
tadashi-aikawa <syou.maman@gmail.com>
2020-07-17 09:03:04 +0900

    🐛 Fix import

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { ValueObject, PrimitiveValueObject } from "./vo";
 import { BaseError } from "./error";
 import { Entity } from "./entity";
+import { DateTime } from "./bundle/datetime";
 
-export { ValueObject, PrimitiveValueObject, Entity, BaseError };
+export { ValueObject, PrimitiveValueObject, Entity, BaseError, DateTime };
 export * from "./either";
