55a71e168db13cb70bdd479d3b3c084ba753a311
tadashi-aikawa <syou.maman@gmail.com>
2021-09-05 15:50:28 +0900

    Fix test failure

--- src/bundle/values.spec.ts
+++ src/bundle/values.spec.ts
@@ -194,7 +194,7 @@ describe("LiquidValue", () => {
       const promise = v.load(async () => err(new BaseError("error")), {
         clearValueBeforeLoading: true,
       });
-      expectLiquidValue(v, "", false);
+      expectLiquidValue(v, "", true);
       await promise;
       expectLiquidValue(v, "", false, "error");
     });
